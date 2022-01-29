89f1599b1f390d903ff553d1cedf4e7848f05625
mrsid_18 <siddharthamehta18@gmail.com>
2022-01-29 16:35:37 -0700

    Updates

---
+++ precache-manifest.5a6818a1629a19d669c2cf59626b04bc.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b525e10ca89cd55e4abcd901c7be07dc",
+    "url": "/nftredzone-v2/index.html"
+  },
+  {
+    "revision": "fc662e3a6dd9866dd56c",
+    "url": "/nftredzone-v2/static/css/main.f938068c.chunk.css"
+  },
+  {
+    "revision": "0eea252df886d4b5dcbe",
+    "url": "/nftredzone-v2/static/js/2.eb28af11.chunk.js"
+  },
+  {
+    "revision": "fc662e3a6dd9866dd56c",
+    "url": "/nftredzone-v2/static/js/main.e8e28113.chunk.js"
+  },
+  {
+    "revision": "3b26e150d9ae42aa74ff",
+    "url": "/nftredzone-v2/static/js/runtime~main.09ce9d74.js"
+  },
+  {
+    "revision": "18fd6dd49ea52603a585a8041c66e0ab",
+    "url": "/nftredzone-v2/static/media/Card-Head Coach.18fd6dd4.jpg"
+  },
+  {
+    "revision": "35b14401888a59a53d99bdd8c2360eee",
+    "url": "/nftredzone-v2/static/media/Card-Ponies-Flex.35b14401.jpg"
+  },
+  {
+    "revision": "b42dbdc0b1331e4505658e93bbf478cd",
+    "url": "/nftredzone-v2/static/media/Logo.b42dbdc0.png"
+  },
+  {
+    "revision": "7f8ac268d6633058ffe8dd11b5859146",
+    "url": "/nftredzone-v2/static/media/bunchu.7f8ac268.png"
+  },
+  {
+    "revision": "3437c10597c1526c3dbd98c737c2bcae",
+    "url": "/nftredzone-v2/static/media/discord.3437c105.svg"
+  },
+  {
+    "revision": "cd7bbd3829d4102aa54ae4472e2625db",
+    "url": "/nftredzone-v2/static/media/gcg.cd7bbd38.PNG"
+  },
+  {
+    "revision": "b7d8a0e95e8d27b2888436b2b9dee97a",
+    "url": "/nftredzone-v2/static/media/home1.b7d8a0e9.jpg"
+  },
+  {
+    "revision": "f7f7c85fd7cf873189768fe7ec22350b",
+    "url": "/nftredzone-v2/static/media/home2.f7f7c85f.jpg"
+  },
+  {
+    "revision": "fc3ce2f7936d5a003a408187ac051ca8",
+    "url": "/nftredzone-v2/static/media/illustration-element-01-mobile.fc3ce2f7.svg"
+  },
+  {
+    "revision": "83f765d7b043b05c1c1c511c3472e87f",
+    "url": "/nftredzone-v2/static/media/illustration-element-01.83f765d7.svg"
+  },
+  {
+    "revision": "906041191e40425990253ed776d9ddf1",
+    "url": "/nftredzone-v2/static/media/illustration-element-02.90604119.svg"
+  },
+  {
+    "revision": "9db7925c3f2d02d681e0c3f3c8c387eb",
+    "url": "/nftredzone-v2/static/media/illustration-element-03.9db7925c.svg"
+  },
+  {
+    "revision": "1dacf85fdce9b15d992dfafa21c6dd6d",
+    "url": "/nftredzone-v2/static/media/illustration-element-04.1dacf85f.svg"
+  },
+  {
+    "revision": "d2183cc0334548f78218645c1971975c",
+    "url": "/nftredzone-v2/static/media/illustration-element-05.d2183cc0.svg"
+  },
+  {
+    "revision": "f2bdaf928f823a14d7afd3b5af46b9ea",
+    "url": "/nftredzone-v2/static/media/illustration-element-06.f2bdaf92.svg"
+  },
+  {
+    "revision": "26569773825a18f09a76caf9e7a55b2c",
+    "url": "/nftredzone-v2/static/media/illustration-element-07.26569773.svg"
+  },
+  {
+    "revision": "ec01731a04187ac902e592ddda3148b6",
+    "url": "/nftredzone-v2/static/media/illustration-element-08.ec01731a.svg"
+  },
+  {
+    "revision": "5d337d2c4fc0af1baee865d11d5a0bca",
+    "url": "/nftredzone-v2/static/media/illustration-element-09.5d337d2c.svg"
+  },
+  {
+    "revision": "d15e3b9a542d786cf4fde24edf38a11b",
+    "url": "/nftredzone-v2/static/media/illustration-element-10.d15e3b9a.svg"
+  },
+  {
+    "revision": "3d05e5031bb1232e82aa7d733fd4e2f6",
+    "url": "/nftredzone-v2/static/media/illustration-section-01.3d05e503.svg"
+  },
+  {
+    "revision": "d3c914513998b6f7e4fc618c6cafc2a3",
+    "url": "/nftredzone-v2/static/media/illustration-section-02.d3c91451.svg"
+  },
+  {
+    "revision": "d84d99ba2c0ba812efbf77bf62dac7ff",
+    "url": "/nftredzone-v2/static/media/illustration-section-03.d84d99ba.svg"
+  },
+  {
+    "revision": "2750c0f975f1638fe37149a5cb92549c",
+    "url": "/nftredzone-v2/static/media/illustration-section-04.2750c0f9.svg"
+  },
+  {
+    "revision": "8fbd3b298cb146e2329b576e0c06a62f",
+    "url": "/nftredzone-v2/static/media/premium.8fbd3b29.png"
+  },
+  {
+    "revision": "70eb74b2d2973aa89c3d2dc03f2be81c",
+    "url": "/nftredzone-v2/static/media/prize.70eb74b2.png"
+  },
+  {
+    "revision": "c7207f170d2d6e0b28059e9ac0965cce",
+    "url": "/nftredzone-v2/static/media/roadmap.c7207f17.gif"
+  },
+  {
+    "revision": "3658910e566e5f7668606533afb9a3ac",
+    "url": "/nftredzone-v2/static/media/schedule.3658910e.png"
+  },
+  {
+    "revision": "7fab00cad29bd7197a9542d298b3c492",
+    "url": "/nftredzone-v2/static/media/spike.7fab00ca.svg"
+  },
+  {
+    "revision": "f60435e9dc85d2411b7bb39745fee882",
+    "url": "/nftredzone-v2/static/media/twitter.f60435e9.svg"
+  },
+  {
+    "revision": "36e2b133f0ceb76a87596672183253f4",
+    "url": "/nftredzone-v2/static/media/zwingli.36e2b133.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/nftredzone-v2/precache-manifest.5a6818a1629a19d669c2cf59626b04bc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nftredzone-v2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
